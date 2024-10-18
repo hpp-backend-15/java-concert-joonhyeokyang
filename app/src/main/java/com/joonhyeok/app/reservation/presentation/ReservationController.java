e94827d34baf757fc0a989301cff238902c8e867
uicheon <highestbright98@naver.com>
2024-10-19 00:58:15 +0900

    test: 예약 도메인 컨트롤러 - 서비스 연결

--- app/src/main/java/com/joonhyeok/app/reservation/presentation/ReservationController.java
+++ app/src/main/java/com/joonhyeok/app/reservation/presentation/ReservationController.java
@@ -1,12 +1,18 @@
 package com.joonhyeok.app.reservation.presentation;
 
 import com.joonhyeok.app.common.aop.token.VerifyWait;
+import com.joonhyeok.app.reservation.application.MakeReservationService;
+import com.joonhyeok.app.reservation.application.dto.MakeReservationCommand;
+import com.joonhyeok.app.reservation.application.dto.MakeReservationResult;
 import com.joonhyeok.openapi.apis.ReservationApi;
 import com.joonhyeok.openapi.models.ReservationResponse;
 import com.joonhyeok.openapi.models.ReservationsPostRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 
+@RequiredArgsConstructor
 public class ReservationController implements ReservationApi {
+    private final MakeReservationService makeReservationService;
 
     @Override
     @VerifyWait
@@ -14,6 +20,11 @@ public class ReservationController implements ReservationApi {
             String waitToken,
             ReservationsPostRequest reservationsPostRequest
     ) {
-        return ResponseEntity.ok(new ReservationResponse());
+        MakeReservationResult result = makeReservationService.reserve(
+                new MakeReservationCommand(
+                                reservationsPostRequest.getSeatId(),
+                                reservationsPostRequest.getUserId())
+        );
+        return ResponseEntity.ok(new ReservationResponse().reservationId(result.reservationId()));
     }
 }
