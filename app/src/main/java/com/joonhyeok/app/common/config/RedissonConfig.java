316ae248c3fd27ed5dd8834c3b1f44b68639e5ad
uicheon <highestbright98@naver.com>
2024-11-01 02:59:20 +0900

    Redisson 주소 변경

--- app/src/main/java/com/joonhyeok/app/common/config/RedissonConfig.java
+++ app/src/main/java/com/joonhyeok/app/common/config/RedissonConfig.java
@@ -17,13 +17,13 @@ public class RedissonConfig {
     @Value("${spring.data.redis.port}")
     private int redisPort;
 
-    private static final String REDISSON_HOST_PREFIX = "redis://";
+    private static final String REDISSON_HOST_PREFIX = "rediss://";
 
     @Bean
     public RedissonClient redissonClient() {
         RedissonClient redisson = null;
         Config config = new Config();
-        config.useSingleServer().setAddress(redisHost + ":" + redisPort);
+        config.useSingleServer().setAddress(REDISSON_HOST_PREFIX + redisHost + ":" + redisPort);
         redisson = Redisson.create(config);
         return redisson;
     }
