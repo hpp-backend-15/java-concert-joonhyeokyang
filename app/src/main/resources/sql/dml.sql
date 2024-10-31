dda0525a0e3b83f41c706472acb8679b4792f85c
uicheon <highestbright98@naver.com>
2024-11-01 05:11:52 +0900

    좌석 양을 늘려서 테스트

--- app/src/main/resources/sql/dml.sql
+++ app/src/main/resources/sql/dml.sql
@@ -31,156 +31,55 @@ INSERT INTO "concert-test".queues VALUES (NULL, NULL, 1, '2024-10-31 21:26:19.37
 -- Data for Name: seat; Type: TABLE DATA; Schema: concert-test; Owner: postgres
 --
 
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 1, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 2, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 3, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 4, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 5, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 6, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 7, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 8, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 9, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 10, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 11, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 12, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 13, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 14, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 15, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 16, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 17, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 18, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 19, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 20, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 21, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 22, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 23, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 24, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 25, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 26, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 27, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 28, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 29, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 30, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 31, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 32, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 33, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 34, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 35, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 36, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 37, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 38, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 39, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 40, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 41, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 42, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 43, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 44, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 45, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 46, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 47, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 48, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 49, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 1, 50, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 51, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 52, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 53, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 54, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 55, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 56, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 57, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 58, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 59, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 60, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 61, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 62, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 63, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 64, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 65, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 66, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 67, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 68, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 69, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 70, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 71, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 72, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 73, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 74, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 75, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 76, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 77, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 78, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 79, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 80, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 81, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 82, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 83, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 84, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 85, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 86, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 87, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 88, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 89, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 90, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 91, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 92, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 93, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 94, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 95, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 96, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 97, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 98, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 99, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 2, 100, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 101, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 102, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 103, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 104, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 105, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 106, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 107, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 108, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 109, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 110, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 111, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 112, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 113, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 114, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 115, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 116, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 117, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 118, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 119, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 120, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 121, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 122, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 123, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 124, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 125, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 126, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 127, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 128, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 129, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 130, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 131, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 132, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 133, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 134, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 135, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 136, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 137, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 138, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 139, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 140, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 141, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 142, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 143, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 144, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 145, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 146, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 147, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 148, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 149, 0, 'AVAILABLE');
-INSERT INTO "concert-test".seat VALUES (0, NULL, 3, 150, 0, 'AVAILABLE');
+--
+-- Data for Name: concert; Type: TABLE DATA; Schema: concert-test; Owner: postgres
+--
+
+INSERT INTO "concert-test".concert VALUES (1, 'joonhyeok');
+
+
+--
+-- Data for Name: performance_date; Type: TABLE DATA; Schema: concert-test; Owner: postgres
+--
+
+INSERT INTO "concert-test".performance_date VALUES ('2024-10-31', 1, 1);
+INSERT INTO "concert-test".performance_date VALUES ('2024-11-01', 1, 2);
+INSERT INTO "concert-test".performance_date VALUES ('2024-11-02', 1, 3);
+
+
+--
+-- Data for Name: queues; Type: TABLE DATA; Schema: concert-test; Owner: postgres
+--
+
+INSERT INTO "concert-test".queues VALUES (NULL, NULL, 1, '2024-10-31 21:26:19.371355', NULL, 1, '1', 'ACTIVE');
+
+
+--
+-- Data for Name: reservation; Type: TABLE DATA; Schema: concert-test; Owner: postgres
+--
+
+
+
+--
+-- Data for Name: seat; Type: TABLE DATA; Schema: concert-test; Owner: postgres
+--
+-- performanceId 1: 1부터 60,666개
+INSERT INTO "concert-test".seat (version, last_reserved_at, performance_dates_id, seats_id, seats_price, seats_status)
+SELECT 0 AS version, NULL AS modified, 1 AS performanceId,
+       generate_series(1, 60666) AS seats_id,
+       0 AS price, 'AVAILABLE' AS status
+UNION ALL
+
+-- performanceId 2: 60,667부터 80,000개
+SELECT 0 AS version, NULL AS modified, 2 AS performanceId,
+       generate_series(60667, 80000) AS seats_id,
+       0 AS price, 'AVAILABLE' AS status
+UNION ALL
+
+-- performanceId 3: 80,001부터 100,000개
+SELECT 0 AS version, NULL AS modified, 3 AS performanceId,
+       generate_series(80001, 100000) AS seats_id,
+       0 AS price, 'AVAILABLE' AS status;
 
 
 --
