53edf3bba8c6022249bdc24a411a65f17234bf50
uicheon <highestbright98@naver.com>
2024-11-03 16:30:12 +0900

    Redisson Wait, Lease 수정

--- app/src/main/java/com/joonhyeok/app/reservation/infra/RedissonLockManager.java
+++ app/src/main/java/com/joonhyeok/app/reservation/infra/RedissonLockManager.java
@@ -13,8 +13,8 @@ import java.util.concurrent.TimeUnit;
 @RequiredArgsConstructor
 @Component
 public class RedissonLockManager implements LockManager {
-    long waitTime = 300L;
-    long leaseTime = 75L;
+    long waitTime = 900L;
+    long leaseTime = 300L;
     TimeUnit timeUnit = TimeUnit.MICROSECONDS;
     private final RedissonClient redissonClient;
 
