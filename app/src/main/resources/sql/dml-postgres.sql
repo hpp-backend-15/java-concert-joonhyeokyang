7d05acaf0258a7f7468b0cb7a5e55f977aae39d8
uicheon <highestbright98@naver.com>
2024-11-06 23:10:56 +0900

    application.yaml 수정

---
+++ app/src/main/resources/sql/dml-postgres.sql
@@ -0,0 +1,58 @@
+--
+-- Data for Name: concert; Type: TABLE DATA; Schema: concert-test; Owner: postgres
+--
+
+INSERT INTO "concert-test".concert VALUES (1, 'joonhyeok');
+
+
+--
+-- Data for Name: performance_date; Type: TABLE DATA; Schema: concert-test; Owner: postgres
+--
+
+INSERT INTO "concert-test".performance_date VALUES ('2024-10-31', 1, 1);
+INSERT INTO "concert-test".performance_date VALUES ('2024-11-01', 1, 2);
+INSERT INTO "concert-test".performance_date VALUES ('2024-11-02', 1, 3);
+
+
+--
+-- Data for Name: queues; Type: TABLE DATA; Schema: concert-test; Owner: postgres
+--
+
+INSERT INTO "concert-test".queues VALUES (NULL, NULL, 1, '2024-10-31 21:26:19.371355', NULL, 1, '1', 'ACTIVE');
+
+
+--
+-- Data for Name: reservation; Type: TABLE DATA; Schema: concert-test; Owner: postgres
+--
+
+
+
+--
+-- Data for Name: seat; Type: TABLE DATA; Schema: concert-test; Owner: postgres
+--
+-- performanceId 1: 1부터 33,333개
+INSERT INTO "concert-test".seat (version, last_reserved_at, performance_dates_id, seats_id, seats_price, seats_status)
+SELECT 0 AS version, CAST(NULL AS timestamp) AS last_reserved_at, 1 AS performanceId,
+       generate_series(1, 33333) AS seats_id,
+       0 AS price, 'AVAILABLE' AS status
+UNION ALL
+
+-- performanceId 2: 33,334부터 66,667개
+SELECT 0 AS version, CAST(NULL AS timestamp) AS last_reserved_at, 2 AS performanceId,
+       generate_series(33334, 66667) AS seats_id,
+       0 AS price, 'AVAILABLE' AS status
+UNION ALL
+
+-- performanceId 3: 66,668부터 100,000개
+SELECT 0 AS version, CAST(NULL AS timestamp) AS last_reserved_at, 3 AS performanceId,
+       generate_series(66668, 100000) AS seats_id,
+       0 AS price, 'AVAILABLE' AS status;
+
+
+
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: concert-test; Owner: postgres
+--
+
+INSERT INTO "concert-test".users VALUES (0, 0, '2024-10-31 21:26:19.371344', 1);
--- app/src/main/resources/sql/dml.sql
+++ app/src/main/resources/sql/dml.sql
@@ -1,12 +1,12 @@
 --
--- Data for Name: concert; Type: TABLE DATA; Schema: concert; Owner: postgres
+-- Data for Name: concert; Type: TABLE DATA; Schema: concert-test; Owner: postgres
 --
 
 INSERT INTO concert VALUES (1, 'joonhyeok');
 
 
 --
--- Data for Name: performance_date; Type: TABLE DATA; Schema: concert; Owner: postgres
+-- Data for Name: performance_date; Type: TABLE DATA; Schema: concert-test; Owner: postgres
 --
 
 INSERT INTO performance_date VALUES ('2024-10-31', 1, 1);
@@ -15,44 +15,53 @@ INSERT INTO performance_date VALUES ('2024-11-02', 1, 3);
 
 
 --
--- Data for Name: queues; Type: TABLE DATA; Schema: concert; Owner: postgres
+-- Data for Name: queues; Type: TABLE DATA; Schema: concert-test; Owner: postgres
 --
 
 INSERT INTO queues VALUES (NULL, NULL, 1, '2024-10-31 21:26:19.371355', NULL, 1, '1', 'ACTIVE');
 
 
 --
--- Data for Name: reservation; Type: TABLE DATA; Schema: concert; Owner: postgres
+-- Data for Name: reservation; Type: TABLE DATA; Schema: concert-test; Owner: postgres
 --
 
 
 
 --
--- Data for Name: seat; Type: TABLE DATA; Schema: concert; Owner: postgres
+-- Data for Name: seat; Type: TABLE DATA; Schema: concert-test; Owner: postgres
 --
 -- performanceId 1: 1부터 33,333개
-INSERT INTO seat (version, last_reserved_at, performance_dates_id, seats_id, seats_price, seats_status)
-SELECT 0 AS version, CAST(NULL AS timestamp) AS last_reserved_at, 1 AS performanceId,
-       generate_series(1, 33333) AS seats_id,
-       0 AS price, 'AVAILABLE' AS status
-UNION ALL
+DELIMITER $$
 
--- performanceId 2: 33,334부터 66,667개
-SELECT 0 AS version, CAST(NULL AS timestamp) AS last_reserved_at, 2 AS performanceId,
-       generate_series(33334, 66667) AS seats_id,
-       0 AS price, 'AVAILABLE' AS status
-UNION ALL
+CREATE PROCEDURE loopInsert()
+BEGIN
+    DECLARE i INT DEFAULT 1;
+    DECLARE performance_dates_id INT DEFAULT 1;
 
--- performanceId 3: 66,668부터 100,000개
-SELECT 0 AS version, CAST(NULL AS timestamp) AS last_reserved_at, 3 AS performanceId,
-       generate_series(66668, 100000) AS seats_id,
-       0 AS price, 'AVAILABLE' AS status;
+    WHILE i <= 3000 DO
+        -- performance_dates_id 값 설정
+        IF i <= 1000 THEN
+            SET performance_dates_id = 1;
+        ELSEIF i <= 2000 THEN
+            SET performance_dates_id = 2;
+ELSE
+            SET performance_dates_id = 3;
+END IF;
 
+        -- 데이터 삽입
+INSERT INTO concert.seat (version, last_reserved_at, performance_dates_id, seats_price, seats_status)
+VALUES (0, NULL, performance_dates_id, 0, 'AVAILABLE');
 
+-- 카운터 증가
+SET i = i + 1;
+END WHILE;
+END $$
 
-
+DELIMITER ;
 --
--- Data for Name: users; Type: TABLE DATA; Schema: concert; Owner: postgres
+-- Data for Name: users; Type: TABLE DATA; Schema: concert-test; Owner: postgres
 --
 
 INSERT INTO users VALUES (0, 0, '2024-10-31 21:26:19.371344', 1);
+
+CALL loopInsert();
