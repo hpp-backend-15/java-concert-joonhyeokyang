afdd4b696404524f06135506fae53d68068cea08
uicheon <highestbright98@naver.com>
2024-11-06 16:30:17 +0900

    dml 수정

--- app/src/main/resources/sql/dml.sql
+++ app/src/main/resources/sql/dml.sql
@@ -1,37 +1,37 @@
 --
--- Data for Name: concert; Type: TABLE DATA; Schema: concert-test; Owner: postgres
+-- Data for Name: concert; Type: TABLE DATA; Schema: concert; Owner: postgres
 --
 
-INSERT INTO "concert-test".concert VALUES (1, 'joonhyeok');
-
-
---
--- Data for Name: performance_date; Type: TABLE DATA; Schema: concert-test; Owner: postgres
---
-
-INSERT INTO "concert-test".performance_date VALUES ('2024-10-31', 1, 1);
-INSERT INTO "concert-test".performance_date VALUES ('2024-11-01', 1, 2);
-INSERT INTO "concert-test".performance_date VALUES ('2024-11-02', 1, 3);
+INSERT INTO "concert".concert VALUES (1, 'joonhyeok');
 
 
 --
--- Data for Name: queues; Type: TABLE DATA; Schema: concert-test; Owner: postgres
+-- Data for Name: performance_date; Type: TABLE DATA; Schema: concert; Owner: postgres
 --
 
-INSERT INTO "concert-test".queues VALUES (NULL, NULL, 1, '2024-10-31 21:26:19.371355', NULL, 1, '1', 'ACTIVE');
-
-
---
--- Data for Name: reservation; Type: TABLE DATA; Schema: concert-test; Owner: postgres
---
-
+INSERT INTO "concert".performance_date VALUES ('2024-10-31', 1, 1);
+INSERT INTO "concert".performance_date VALUES ('2024-11-01', 1, 2);
+INSERT INTO "concert".performance_date VALUES ('2024-11-02', 1, 3);
 
 
 --
--- Data for Name: seat; Type: TABLE DATA; Schema: concert-test; Owner: postgres
+-- Data for Name: queues; Type: TABLE DATA; Schema: concert; Owner: postgres
+--
+
+INSERT INTO "concert".queues VALUES (NULL, NULL, 1, '2024-10-31 21:26:19.371355', NULL, 1, '1', 'ACTIVE');
+
+
+--
+-- Data for Name: reservation; Type: TABLE DATA; Schema: concert; Owner: postgres
+--
+
+
+
+--
+-- Data for Name: seat; Type: TABLE DATA; Schema: concert; Owner: postgres
 --
 -- performanceId 1: 1부터 33,333개
-INSERT INTO "concert-test".seat (version, last_reserved_at, performance_dates_id, seats_id, seats_price, seats_status)
+INSERT INTO "concert".seat (version, last_reserved_at, performance_dates_id, seats_id, seats_price, seats_status)
 SELECT 0 AS version, CAST(NULL AS timestamp) AS last_reserved_at, 1 AS performanceId,
        generate_series(1, 33333) AS seats_id,
        0 AS price, 'AVAILABLE' AS status
@@ -52,7 +52,7 @@ SELECT 0 AS version, CAST(NULL AS timestamp) AS last_reserved_at, 3 AS performan
 
 
 --
--- Data for Name: users; Type: TABLE DATA; Schema: concert-test; Owner: postgres
+-- Data for Name: users; Type: TABLE DATA; Schema: concert; Owner: postgres
 --
 
-INSERT INTO "concert-test".users VALUES (0, 0, '2024-10-31 21:26:19.371344', 1);
+INSERT INTO "concert".users VALUES (0, 0, '2024-10-31 21:26:19.371344', 1);
