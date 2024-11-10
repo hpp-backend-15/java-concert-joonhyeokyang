6bc4d9fadef677e5be23cd5c2d38e77b04c0c961
uicheon <highestbright98@naver.com>
2024-11-10 12:49:47 +0900

    롤백

--- app/src/main/java/com/joonhyeok/app/reservation/presentation/ReservationController.java
+++ app/src/main/java/com/joonhyeok/app/reservation/presentation/ReservationController.java
@@ -28,8 +28,8 @@ public class ReservationController implements ReservationApi {
     public ResponseEntity<ReservationResponse> makeReservation(String waitToken, MakeReservationRequest request) {
         MakeReservationResult result = null;
         LockId lock = null;
-//        try {
-//            lock = lockManager.tryLock("seat", request.getSeatId());
+        try {
+            lock = lockManager.tryLock("seat", request.getSeatId());
         result = makeReservationService.reserve(
                 new MakeReservationCommand(
                         request.getConcertId(),
@@ -37,12 +37,12 @@ public class ReservationController implements ReservationApi {
                         request.getSeatId(),
                         request.getUserId()))
         ;
-//        } catch (Exception e) {
-//            log.error(e.getMessage());
-//            e.printStackTrace();
-//        } finally {
-//            lockManager.releaseLock(lock);
-//        }
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            e.printStackTrace();
+        } finally {
+            lockManager.releaseLock(lock);
+        }
         return ResponseEntity.status(HttpStatus.CREATED).body(new ReservationResponse().reservationId(result.reservationId()));
     }
 }
