f8eee1556d4f7f3f2ef440ed0f662653fad7c1e3
uicheon <highestbright98@naver.com>
2024-11-12 23:25:17 +0900

    7주차 피드백 적용 (GenericJackson2JsonRedisSerializer ->StringRedisSerializer)

--- app/src/main/java/com/joonhyeok/app/common/config/RedisCacheConfig.java
+++ app/src/main/java/com/joonhyeok/app/common/config/RedisCacheConfig.java
@@ -8,6 +8,7 @@ import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -21,7 +22,7 @@ public class RedisCacheConfig {
     public CacheManager contentCacheManager(RedisConnectionFactory cf) {
         RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
                 .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
-                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer())) // Value Serializer 변경
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer())) // Value Serializer 변경
                 .entryTtl(Duration.ofMinutes(3L)); // 캐시 수명 30분
 
         return RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(cf).cacheDefaults(redisCacheConfiguration).build();
