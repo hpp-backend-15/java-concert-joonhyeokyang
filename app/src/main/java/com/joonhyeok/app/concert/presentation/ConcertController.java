7902ae95eda2db3b0c8ea9e0a8bb8825a7735c78
uicheon <highestbright98@naver.com>
2024-11-13 00:05:49 +0900

    7주차 피드백 적용 주석 삭제

--- app/src/main/java/com/joonhyeok/app/concert/presentation/ConcertController.java
+++ app/src/main/java/com/joonhyeok/app/concert/presentation/ConcertController.java
@@ -19,8 +19,6 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class ConcertController implements ConcertApi {
     private final ConcertService concertService;
-    private final PerformanceDateMapper performanceDateMapper;
-    private final SeatMapper seatMapper;
 
     @Override
     @VerifyWait
@@ -31,9 +29,6 @@ public class ConcertController implements ConcertApi {
         PerformanceDatesQueryResult result =
                 concertService.queryPerformanceDates(new AvailablePerformanceDatesQuery(concertId));
 
-//        FindConcertPerformanceDatesResponse response = new FindConcertPerformanceDatesResponse()
-//                .availablePerformanceDates(performanceDateMapper.toPresentationList(result.availableDates()))
-//                .unavailablePerformanceDates(performanceDateMapper.toPresentationList(result.unavailableDates()));
         FindConcertPerformanceDatesResponse response = new FindConcertPerformanceDatesResponse()
                 .availablePerformanceDates(result.availableDates().stream().map(pd -> new PerformanceDateResponse().date(pd.getPerformanceDate())).toList())
                 .unavailablePerformanceDates(result.unavailableDates().stream().map(pd -> new PerformanceDateResponse().date(pd.getPerformanceDate())).toList());
@@ -49,9 +44,6 @@ public class ConcertController implements ConcertApi {
             @PathVariable(value = "performanceDateId") Long performanceDateId
     ) {
         SeatsQueryResult result = concertService.querySeatsByDate(new AvailableSeatsByDateQuery(concertId, performanceDateId));
-//        FindConcertAvailableSeatsResponse response = new FindConcertAvailableSeatsResponse()
-//                .availableSeats(seatMapper.toPresentationList(result.availableSeats()))
-//                .unavailableSeats(seatMapper.toPresentationList(result.unavailableSeats()));
         FindConcertAvailableSeatsResponse response = new FindConcertAvailableSeatsResponse()
                 .availableSeats(result.availableSeats().stream().map(
                         seat -> new SeatResponse()
