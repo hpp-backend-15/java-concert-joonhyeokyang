ea9ed31efb3c57317338b273692e4eda97db97cd
uicheon <highestbright98@naver.com>
2024-11-13 16:05:00 +0900

    change: redis 캐슁시, 클래스정보 없도록 변경

--- app/src/main/java/com/joonhyeok/app/common/config/RedisCacheConfig.java
+++ app/src/main/java/com/joonhyeok/app/common/config/RedisCacheConfig.java
@@ -1,5 +1,12 @@
 package com.joonhyeok.app.common.config;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
@@ -7,6 +14,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -15,12 +23,30 @@ import java.time.Duration;
 @Configuration
 @EnableCaching
 public class RedisCacheConfig {
+    @Bean
+    public ObjectMapper objectMapper() {
+        BasicPolymorphicTypeValidator validator = BasicPolymorphicTypeValidator.builder()
+                .allowIfSubType(Object.class)
+                .build();
+
+        ObjectMapper objectMapper = new JsonMapper();
+        objectMapper
+                .registerModule(new JavaTimeModule())
+                .setPolymorphicTypeValidator(validator)
+                .configure(MapperFeature.USE_ANNOTATIONS, false)
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                .configure(MapperFeature.USE_GETTERS_AS_SETTERS, false)
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+        return objectMapper;
+    }
+
 
     @Bean
-    public CacheManager contentCacheManager(RedisConnectionFactory cf) {
+    public CacheManager contentCacheManager(RedisConnectionFactory cf, ObjectMapper objectMapper) {
         RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
                 .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
-                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer(objectMapper)))
                 .entryTtl(Duration.ofMinutes(30L));
 
         return RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(cf).cacheDefaults(redisCacheConfiguration).build();
