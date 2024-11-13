2299ec0c837042c5fe3bd2382207bfa45d1fb39c
uicheon <highestbright98@naver.com>
2024-11-13 16:04:28 +0900

    change: redis 캐쉬가 @Transactional 보다 먼저 수행되게끔 변경

--- app/src/main/java/com/joonhyeok/app/common/aop/token/VerifyWait.java
+++ app/src/main/java/com/joonhyeok/app/common/aop/token/VerifyWait.java
@@ -1,10 +1,15 @@
 package com.joonhyeok.app.common.aop.token;
 
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+
+@Order(Ordered.LOWEST_PRECEDENCE-1)
 @Target(ElementType.METHOD) // 메서드에만 적용
 @Retention(RetentionPolicy.RUNTIME) // 런타임까지 유지
 public @interface VerifyWait {
