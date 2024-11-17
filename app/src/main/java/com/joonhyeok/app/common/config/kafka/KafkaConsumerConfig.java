c730d6948b89c0159a9624a2b75c867060102ac6
uicheon <highestbright98@naver.com>
2024-11-17 14:46:39 +0900

    feat: kafka 의존성, 설정 추가

---
+++ app/src/main/java/com/joonhyeok/app/common/config/kafka/KafkaConsumerConfig.java
@@ -0,0 +1,67 @@
+package com.joonhyeok.app.common.config.kafka;
+
+import com.fasterxml.jackson.databind.deser.std.StringDeserializer;
+import com.joonhyeok.app.user.domain.PayEvent;
+import com.joonhyeok.app.user.infra.domain.payEvent.PayEventDeserializer;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
+import org.springframework.util.backoff.FixedBackOff;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@EnableKafka
+@Configuration
+public class KafkaConsumerConfig {
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServer;
+
+    @Value("${spring.kafka.consumer.group-id}")
+    private String groupId;
+
+    @Bean
+    public ConsumerFactory<String, PayEvent> consumerPayEventFactory() {
+        Map<String, Object> properties = new HashMap<>();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
+        properties.put(ErrorHandlingDeserializer.KEY_DESERIALIZER_CLASS, StringDeserializer.class);
+        properties.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, PayEventDeserializer.class);
+        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+        return new DefaultKafkaConsumerFactory<>(properties);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, PayEvent>
+    EventListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, PayEvent> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setCommonErrorHandler(customErrorHandler());
+        factory.setConsumerFactory(consumerPayEventFactory());
+        return factory;
+    }
+
+
+    private DefaultErrorHandler customErrorHandler() {
+        DefaultErrorHandler errorHandler = new DefaultErrorHandler((consumerRecord, exception) ->
+                log.error(
+                        "[Error] topic = {}, key = {}, value = {}, error message = {}",
+                        consumerRecord.topic(),
+                        consumerRecord.key(),
+                        consumerRecord.value(),
+                        exception.getMessage()), new FixedBackOff(1000L, 5));
+        errorHandler.addNotRetryableExceptions(RuntimeException.class);
+        return errorHandler;
+    }
+}
